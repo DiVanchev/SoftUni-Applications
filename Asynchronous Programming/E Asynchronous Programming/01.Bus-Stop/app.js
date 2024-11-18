66435400f278edd426ebb12832fceb2cc66b8b3d
Dimitar Vachev <d.vachev2002@gmail.com>
2024-11-18 12:38:44 +0200

    update

---
+++ Asynchronous Programming/E Asynchronous Programming/01.Bus-Stop/app.js
@@ -0,0 +1,26 @@
+async function getInfo() {
+    const stopNameElement = document.getElementById('stopName');
+    const timeTableElement = document.getElementById('buses');
+    const submitBtn = document.getElementById('submit');
+
+    const stopID = document.getElementById('stopId').value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopID}`;
+
+    try {
+        stopNameElement.textContent = 'Loading...';
+        timeTableElement.replaceChild();
+        submitBtn.disabled = true;
+
+        const res = await fetch(url);
+        
+        if(res.status !== 200) {
+            throw Error('Stop ID is not found!');
+        }
+
+        const data = await res.json();
+
+        console.log(data);
+    } catch {
+
+    }
+}
